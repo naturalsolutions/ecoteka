d531bd676d2012ad26ae3dcd5dcc4a773e8aa597
Javier Blanco García-Lomas <jblancogl@gmail.com>
2020-07-28 15:08:30 +0200

    add hidden component to toolbar.tsx

--- frontend/components/Toolbar.tsx
+++ frontend/components/Toolbar.tsx
@@ -3,6 +3,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
+import Hidden from "@material-ui/core/Hidden";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 
@@ -42,17 +43,19 @@ const ETKToolbar: React.FC<ETKToolbarProps> = (props) => {
           <MenuIcon />
         </IconButton>
         <img src={props.logo} className={classes.logo} />
-        <Typography
-          component="h2"
-          variant="h5"
-          className={classes.numberOfTrees}
-          color="primary"
-        >
-          {props.numberOfTrees}
-        </Typography>
-        <div className={classes.buttons}>
-          <Button color="primary">{props.aboutText}</Button>
-        </div>
+        <Hidden smDown>
+          <Typography
+            component="h2"
+            variant="h5"
+            className={classes.numberOfTrees}
+            color="primary"
+          >
+            {props.numberOfTrees}
+          </Typography>
+          <div className={classes.buttons}>
+            <Button color="primary">{props.aboutText}</Button>
+          </div>
+        </Hidden>
       </Toolbar>
     </AppBar>
   );
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -44,21 +44,5 @@ export default function Index() {
         <div>aaaaa</div>
       </Drawer>
     </React.Fragment>
-    /*  <Container>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">News</Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-      </Box>
-    </Container> */
   );
 }
