a22806ed12bc1f6a07e06ccda8659a0948048e9c
MV <el-makki_voundy@natural-solutions.eu>
2020-07-29 15:05:28 +0200

    open registration for tests

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -72,7 +72,7 @@ class Settings(BaseSettings):
     EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
     FIRST_SUPERUSER: EmailStr
     FIRST_SUPERUSER_PASSWORD: str
-    USERS_OPEN_REGISTRATION: bool = False
+    USERS_OPEN_REGISTRATION: bool = True
 
     class Config:
         case_sensitive = True
