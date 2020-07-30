13dc3b3b1de03400179b7b1a0e878f9005b81022
Javi Blanco <javier_blanco@natural-solutions.eu>
2020-07-30 14:41:53 +0000

    Resolve "Intregation Contact"

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -19,6 +19,10 @@ export default function MyApp(props) {
     <React.Fragment>
       <Head>
         <title>EcoTeka</title>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+        />
       </Head>
       <CssBaseline />
       <Component {...pageProps} />
