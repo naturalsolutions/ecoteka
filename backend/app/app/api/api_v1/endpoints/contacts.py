538bf37bef75ca0360c6dbc823f24a7c3abeb7e0
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-08-12 11:33:23 +0200

    linter fix

--- backend/app/app/api/api_v1/endpoints/contacts.py
+++ backend/app/app/api/api_v1/endpoints/contacts.py
@@ -1,38 +1,40 @@
-from typing import Any, List
+from typing import Any
 
 from fastapi import (
     APIRouter,
     BackgroundTasks,
-    Body,
-    Depends,
-    HTTPException
+    Depends
 )
-from fastapi.encoders import jsonable_encoder
-from pydantic.networks import EmailStr
 from sqlalchemy.orm import Session
 
-from app import crud, models, schemas
+from app.schemas import (
+    Contact,
+    ContactCreate
+)
+from app.crud import (
+    contact
+)
 from app.api import deps
-from app.core.config import settings
 from app.utils import send_contact_request_confirmation
 
 router = APIRouter()
 
-@router.post("/",response_model=schemas.Contact)
+
+@router.post("/", response_model=Contact)
 async def post_contact_request(
     *,
     db: Session = Depends(deps.get_db),
-    contact_in: schemas.ContactCreate, #provided contact info from the http.post
+    contact_in: ContactCreate,  # provided contact info from the http.post
     background_tasks: BackgroundTasks
 ) -> Any:
     """Submits a contact request"""
-    contact = crud.contact.create(db, obj_in=contact_in)
+    contact_in_db = contact.create(db, obj_in=contact_in)
 
     background_tasks.add_task(
         send_contact_request_confirmation,
-        email_to=contact.email,
-        first_name=contact.first_name,
-        last_name=contact.last_name
+        email_to=contact_in_db.email,
+        first_name=contact_in_db.first_name,
+        last_name=contact_in_db.last_name
     )
 
-    return contact
+    return contact_in_db
