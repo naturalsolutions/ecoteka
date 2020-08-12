f9d0f1670f51a355c8b67af2a2893ecb639c5040
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-08-12 11:36:00 +0200

    linter fix, first superuser added is_verified

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -1,12 +1,19 @@
 from sqlalchemy.orm import Session
 
-from app import crud, schemas
+from app.schemas import (
+    UserCreate
+)
+from app.crud import (
+    user
+)
 from app.core.config import settings
 from app.db import base  # noqa: F401
 
-# make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
+# make sure all SQL Alchemy models
+# are imported (app.db.base) before initializing DB
 # otherwise, SQL Alchemy might fail to initialize relationships properly
-# for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
+# for more details:
+#  https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
 
 
 def init_db(db: Session) -> None:
@@ -15,11 +22,12 @@ def init_db(db: Session) -> None:
     # the tables un-commenting the next line
     # Base.metadata.create_all(bind=engine)
 
-    user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
-    if not user:
-        user_in = schemas.UserCreate(
+    user_in_db = user.get_by_email(db, email=settings.FIRST_SUPERUSER)
+    if not user_in_db:
+        user_in = UserCreate(
             email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
-        user = crud.user.create(db, obj_in=user_in)  # noqa: F841
+        setattr(user_in, 'is_verified', True)
+        user_in_db = user.create(db, obj_in=user_in)  # noqa: F841
