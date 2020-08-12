d913bc1c0cc4302d68667a6a45b087b80b74843e
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-08-12 21:58:27 +0200

    add index package for api

---
+++ backend/app/app/api/__init__.py
@@ -0,0 +1,13 @@
+from .deps import (
+    get_db,
+    get_current_user,
+    get_current_active_user,
+    get_current_active_superuser
+)
+
+__all__ = [
+    "get_db",
+    "get_current_user",
+    "get_current_active_user",
+    "get_current_active_superuser"
+]
