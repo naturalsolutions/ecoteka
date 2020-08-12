25be25b6b30a48d73f62f4b3a8ade28dea368346
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-08-12 11:32:12 +0200

    add index package for import

--- backend/app/app/models/__init__.py
+++ backend/app/app/models/__init__.py
@@ -1,1 +1,15 @@
-from .user import User
+from .contact import (
+    Contact
+)
+from .registration_link import (
+    Registration_Link
+)
+from .user import (
+    User
+)
+
+__all__ = [
+    "Contact",
+    "Registration_Link",
+    "User",
+]
