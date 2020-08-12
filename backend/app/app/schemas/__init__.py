95b884aaf806d233856afb1ac3945f1e105b8b07
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-08-12 11:33:08 +0200

    add index package for import

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -1,4 +1,41 @@
-from .msg import Msg
-from .token import Token, TokenPayload
-from .user import User, UserCreate, UserInDB, UserUpdate
-from .contact import Contact, ContactCreate, ContactUpdate
+from .msg import (
+    Msg
+)
+from .token import (
+    Token,
+    TokenPayload
+)
+from .user import (
+    User,
+    UserCreate,
+    UserInDB,
+    UserUpdate
+)
+from .contact import (
+    Contact,
+    ContactCreate,
+    ContactUpdate
+)
+from .registration_link import (
+    Registration_Link,
+    RegistrationLinkBase,
+    RegistrationLinkCreate,
+    RegistrationLinkUpdate
+)
+
+__all__ = [
+    "Contact",
+    "ContactCreate",
+    "ContactUpdate",
+    "Registration_Link",
+    "RegistrationLinkBase",
+    "RegistrationLinkCreate",
+    "RegistrationLinkUpdate",
+    "Msg",
+    "User",
+    "UserCreate",
+    "UserInDB",
+    "UserUpdate",
+    "Token",
+    "TokenPayload"
+]
