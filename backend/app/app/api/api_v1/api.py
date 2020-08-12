071c6fcd4c4ee30aac174d6dc38480f9d1049d87
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-08-12 11:13:37 +0200

    add it to api router

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,9 +1,35 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import login, users, utils, contacts
+from app.api.api_v1.endpoints import (
+    contacts,
+    login,
+    users,
+    utils,
+    registration_link
+)
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-api_router.include_router(contacts.router, prefix="/contacts", tags=["contacts"])
+api_router.include_router(
+    login.router,
+    tags=["login"]
+)
+api_router.include_router(
+    users.router,
+    prefix="/users",
+    tags=["users"]
+)
+api_router.include_router(
+    utils.router,
+    prefix="/utils",
+    tags=["utils"]
+)
+api_router.include_router(
+    contacts.router,
+    prefix="/contacts",
+    tags=["contacts"]
+)
+api_router.include_router(
+    registration_link.router,
+    prefix="/registration_link",
+    tags=["registration_link"]
+)
