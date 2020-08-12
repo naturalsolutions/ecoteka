924a8c5848c4d39112efd91b92f8f8fa69b3ba1a
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-08-12 11:32:52 +0200

    applies changes from migration script for user

--- backend/app/app/schemas/user.py
+++ backend/app/app/schemas/user.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import datetime
 
 from pydantic import BaseModel, EmailStr
 
@@ -6,9 +7,10 @@ from pydantic import BaseModel, EmailStr
 # Shared properties
 class UserBase(BaseModel):
     email: Optional[EmailStr] = None
-    is_active: Optional[bool] = True
+    status: str = None
     is_superuser: bool = False
     full_name: Optional[str] = None
+    is_verified: Optional[bool] = False
 
 
 # Properties to receive via API on creation
