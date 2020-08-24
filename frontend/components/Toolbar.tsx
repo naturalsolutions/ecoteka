a66505d11cd22f4c527eb734beddfa57e09c8b87
Vincent <vincent_bourgeois@natural-solutions.eu>
2020-08-24 17:15:50 +0200

    WiP Collapsible menu

--- frontend/components/Toolbar.tsx
+++ frontend/components/Toolbar.tsx
@@ -9,6 +9,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import ETKContact from "./Contact";
 
+import Paper from '@material-ui/core/Paper';
+import Collapse from '@material-ui/core/Collapse';
+
 import ETKDarkToggle, { ETKDarkToggleProps } from "./DarkToggle";
 
 export interface ETKToolbarProps {
@@ -31,18 +34,24 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     justifyContent: "flex-end",
   },
+  navBar: {
+    '& button:not(:last-child)': {
+      marginRight: "30px"
+    }
+  },
   numberOfTrees: {
     width: "100%",
-  },
+  }
 }));
 
 const ETKToolbar: React.FC<ETKToolbarProps> = (props) => {
   const classes = useStyles();
 
   const [isContactOpen, setIsContactOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <AppBar className={classes.appBar} position="fixed" color="inherit">
+    <AppBar className={classes.appBar} position="fixed" color="inherit" elevation={isMenuOpen ? 0 : 4}>
       <Toolbar variant="dense">
         <IconButton edge="start" aria-label="menu" onClick={props.onMenuClick}>
           <MenuIcon />
@@ -72,6 +81,56 @@ const ETKToolbar: React.FC<ETKToolbarProps> = (props) => {
           <ETKDarkToggle onToggle={props.onDarkToggle} />
         </div>
       </Toolbar>
+      <Collapse in={isMenuOpen} collapsedHeight={48}>
+        <Paper elevation={4}>
+          <Toolbar className={classes.navBar} variant="dense">
+            <Button
+              color="primary"
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+              }}
+            >
+              PATRIMOINE VEGETAL
+            </Button>
+            <Button
+              color="primary"
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+              }}
+            >
+              INTERVENTIONS
+           </Button>
+            <Button
+              color="primary"
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+              }}
+            >
+              iMPORT DE DONNEES
+           </Button>
+          </Toolbar>
+          <div>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+              sit amet blandit leo lobortis eget.
+          </Typography>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+              sit amet blandit leo lobortis eget.
+          </Typography>
+          </div>
+          <div>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+              sit amet blandit leo lobortis eget.
+          </Typography>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+              sit amet blandit leo lobortis eget.
+          </Typography>
+          </div>
+        </Paper>
+      </Collapse>
       <ETKContact
         isOpen={isContactOpen}
         onClose={() => {
