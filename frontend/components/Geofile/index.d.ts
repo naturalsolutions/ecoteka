23881a9c067141dde6f28a77af0f3b65134b2e6a
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-09-03 20:53:37 +0200

    Create interface for geofile

---
+++ frontend/components/Geofile/index.d.ts
@@ -0,0 +1,18 @@
+export default interface ETKGeofile {
+    id: number;
+    user_id: number;
+    name: string;
+    original_name: string;
+    extension: string;
+    checksum: string;
+    count: 0;
+    crs: string;
+    driver: string;
+    longitude_column: string;
+    latitude_column: string;
+    properties: string;
+    uploaded_date: string;
+    imported_date: string;
+    importing_start: string;
+    public: false;
+}
