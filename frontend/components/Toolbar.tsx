ad471f5c175dbd5fc6a8b29c1201e9c4ebb62c8b
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-09-07 12:02:16 +0200

    changes for handle login and register

--- frontend/components/Toolbar.tsx
+++ frontend/components/Toolbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,11 +9,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import dynamic from "next/dynamic";
 import ETKContact from "./Contact";
+import ETKLogout from './Logout';
+import ETKSignin from './SignIn';
 import ETKDarkToggle, { ETKDarkToggleProps } from "./DarkToggle";
+import Auth from './Auth.js';
 
 export interface ETKToolbarProps {
   logo: string;
   numberOfTrees: string;
+  loginText: string;
+  logoutText: string;
+  registerText: string;
   aboutText: string;
   onMenuClick: React.MouseEventHandler<HTMLElement>;
   onDarkToggle: ETKDarkToggleProps["onToggle"];
@@ -38,12 +44,89 @@ const useStyles = makeStyles((theme) => ({
 
 const ETKToolbar: React.FC<ETKToolbarProps> = (props) => {
   const classes = useStyles();
-  const ETKLogin = dynamic(() => import("../components/Login"), {
+
+  const ETKRegister = dynamic(() => import("../components/Register"), {
     ssr: false
   });
 
-  const [isContactOpen, setIsContactOpen] = useState(false);
+  const { session, setSession } = Auth.useSession()
+  const [ isSigninOpen , setSigninOpen ] = useState(false)
+  const [ isRegisterOpen , setRegisterOpen ] = useState(false)
+  const [ isContactOpen, setIsContactOpen ] = useState(false);
 
+  const renderWhenSession = () =>{
+
+    const checkIsSuperUser = (session: any):boolean => {
+      let token:[string,string,string];
+      let payload:string;
+      let payloadObj: {
+        exp: number,
+        sub: string,
+        is_superuser: boolean
+      };
+      let toRet:boolean = false;
+
+      payloadObj = {
+        exp: -1,
+        sub: '',
+        is_superuser:false
+      }
+
+      if (session.length) {
+        token = session.split('.');
+      }
+      if (token.length) {
+        payload = atob(token[1]);
+      }
+      if (payload) {
+        payloadObj = JSON.parse(payload)
+      }
+      if (payloadObj) {
+        toRet = payloadObj.is_superuser ||  false;
+      }
+
+      return toRet
+    }
+
+    const isSuperUser = checkIsSuperUser(session)
+
+    return(
+      <React.Fragment>
+        <ETKLogout
+          logoutText={props.logoutText}
+        />
+        { isSuperUser &&
+          <Button
+          color="primary"
+          onClick={() => {
+            setSigninOpen(false);
+            setRegisterOpen(true);
+            setIsContactOpen(false);
+          }}
+          >
+            {props.registerText}
+          </Button>
+        }
+
+      </React.Fragment>
+    )
+  }
+  const renderWhenNoSession= () => {
+    return (
+      <React.Fragment>
+        <Hidden xsDown>
+          <Button
+            color="primary"
+            onClick={(e) => {
+              setSigninOpen(true);
+            }}
+          >
+            {props.loginText}
+          </Button>
+        </Hidden>
+      </React.Fragment>
+    )
+  }
   return (
     <AppBar className={classes.appBar} position="fixed" color="inherit">
       <Toolbar variant="dense">
@@ -62,11 +145,16 @@ const ETKToolbar: React.FC<ETKToolbarProps> = (props) => {
           </Typography>
         </Hidden>
         <div className={classes.buttons}>
-          <ETKLogin />
-          <Hidden smDown>
+          { session
+          ? renderWhenSession()
+          : renderWhenNoSession()
+          }
+          <Hidden xsDown>
             <Button
               color="primary"
               onClick={() => {
+                setSigninOpen(false);
+                setRegisterOpen(false);
                 setIsContactOpen(true);
               }}
             >
@@ -76,6 +164,23 @@ const ETKToolbar: React.FC<ETKToolbarProps> = (props) => {
           <ETKDarkToggle onToggle={props.onDarkToggle} />
         </div>
       </Toolbar>
+      <ETKSignin
+        isOpen={isSigninOpen}
+        onClose={ (e) => {
+          setSigninOpen(false)
+        }}
+        onRegisterBtnClick= { (e) => {
+          setSigninOpen(false)
+          setRegisterOpen(true)
+        }}
+      />
+      <ETKRegister
+        isOpen={ isRegisterOpen }
+        onClose= {() => {
+          setRegisterOpen(false)
+        }}
+        submitButtonText="Submit"
+      />
       <ETKContact
         isOpen={isContactOpen}
         onClose={() => {
