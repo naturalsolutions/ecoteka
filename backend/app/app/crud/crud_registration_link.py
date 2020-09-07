17224c8905e98ac46535cd8ef810a61fb4bbd451
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-09-07 11:08:20 +0200

    add mail when generate new registration link

--- backend/app/app/crud/crud_registration_link.py
+++ backend/app/app/crud/crud_registration_link.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from sqlalchemy.orm import Session
-
+from fastapi import BackgroundTasks
 from app.crud import (
     CRUDBase,
     user
@@ -18,7 +18,12 @@ from datetime import (
     datetime,
     timedelta
 )
-from app.core.config import settings
+from app.core import settings
+from app.utils import (
+    generate_registration_link_value,
+    send_new_registration_link_email
+)
+import logging
 
 
 class CRUDRegistrationLink(
@@ -40,32 +45,66 @@ class CRUDRegistrationLink(
         value: str,
         current_user: User
     ) -> Optional[Registration_Link]:
+
         query = db.query(Registration_Link)
-        query = query.filter((
+        query = query.filter(
             Registration_Link.fk_user == current_user.id,
             Registration_Link.value == value
-            )
         )
         registration_link_in_db = query.first()
         if registration_link_in_db is not None:
+            logging.info("registration link ok")
             dateExpiration = (
                 registration_link_in_db.creation_date
                 +
                 timedelta(
-                    days=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS
+                    hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS
                 )
             )
             curDate = datetime.now()
-            if dateExpiration < curDate:
+            logging.info(f"date expirtaion: {dateExpiration}")
+            logging.info(f"date current: {curDate}")
+            if curDate < dateExpiration:
+                logging.info("date ok we validate it")
                 user.update(
                     db=db,
                     db_obj=current_user,
-                    ob_in={'is_verified': True}
+                    obj_in={'is_verified': True}
                 )
         else:
+            logging.info("bla bla bla bla not valid")
             # handle case when expired ?
             registration_link_in_db = None
         return registration_link_in_db
+
+    def generate_and_insert(
+        self,
+        db: Session,
+        fk_user: int,
+        background_tasks: BackgroundTasks
+    ) -> Registration_Link:
+        user_in_db = user.get(db=db, id=fk_user)
+        registration_link_tmp = RegistrationLinkCreate(
+            fk_user=fk_user,
+            value=generate_registration_link_value(),
+            creation_date=datetime.now()
+        )
+        new_registration_link = self.create(
+            db=db,
+            obj_in=registration_link_tmp
+        )
+        db.add(new_registration_link)
+        db.commit()
+        db.refresh(new_registration_link)
+
+        if settings.EMAILS_ENABLED:
+            background_tasks.add_task(
+                send_new_registration_link_email,
+                email_to=user_in_db.email,
+                full_name=user_in_db.full_name,
+                link=new_registration_link.value
+            )
+        return new_registration_link
 
 
 registration_link = CRUDRegistrationLink(Registration_Link)
