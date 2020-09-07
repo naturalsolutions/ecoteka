3661d80e5021b02d88230afb167b54ddfbc911c8
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-09-07 21:55:49 +0200

    name changed

--- backend/app/app/api/__init__.py
+++ backend/app/app/api/__init__.py
@@ -1,13 +1,11 @@
 from .deps import (
     get_db,
     get_current_user,
-    get_current_active_user,
-    get_current_active_superuser
+    get_current_user_if_is_superuser
 )
 
 __all__ = [
     "get_db",
     "get_current_user",
-    "get_current_active_user",
-    "get_current_active_superuser"
+    "get_current_user_if_is_superuser"
 ]
