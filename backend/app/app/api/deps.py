41d600cd073a5a1c3c07a298f78679cbb5f4dad6
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-09-07 10:50:23 +0200

    change method name for superuser and logic for active user

--- backend/app/app/api/deps.py
+++ backend/app/app/api/deps.py
@@ -10,7 +10,15 @@ from jose import jwt
 from pydantic import ValidationError
 from sqlalchemy.orm import Session
 
-from app import crud, models, schemas
+from app.crud import (
+    user
+)
+from app.models import (
+    User
+)
+from app.schemas import (
+    TokenPayload
+)
 from app.core import (
     security,
     settings
@@ -32,36 +40,37 @@ def get_db() -> Generator:
 
 def get_current_user(
     db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
-) -> models.User:
+) -> User:
     try:
         payload = jwt.decode(
             token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
         )
-        token_data = schemas.TokenPayload(**payload)
+        token_data = TokenPayload(**payload)
     except (jwt.JWTError, ValidationError):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
         )
-    user = crud.user.get(db, id=token_data.sub)
-    if not user:
+    user_in_db = user.get(db, id=token_data.sub)
+    if not user_in_db:
         raise HTTPException(status_code=404, detail="User not found")
-    return user
+    return user_in_db
 
 
 def get_current_active_user(
-    current_user: models.User = Depends(get_current_user),
-) -> models.User:
+    current_user: User = Depends(get_current_user),
+) -> User:
     # if not crud.user.is_verified(current_user):
     #     raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+    return user.is_verified(current_user)
 
 
-def get_current_active_superuser(
-    current_user: models.User = Depends(get_current_user),
-) -> models.User:
-    if not crud.user.is_superuser(current_user):
+def get_current_user_if_is_superuser(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    if not user.is_superuser(current_user):
         raise HTTPException(
-            status_code=400, detail="The user doesn't have enough privileges"
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="The user doesn't have enough privileges"
         )
     return current_user
