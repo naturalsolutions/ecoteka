ee84f13c7aaeaa09b487b371bc7897f1fe704191
Javier Blanco <javier_blanco@natural-solutions.eu>
2020-09-08 16:42:17 +0200

    fix: change AGrid to Grid

--- frontend/pages/registration-link/[value]/index.js
+++ frontend/pages/registration-link/[value]/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import Grid from "@material-ui/core/AGrid";
+import Grid from "@material-ui/core/Grid";
 import { useAppContext } from "../../../providers/AppContext";
 import ETKSignin from "../../../components/SignIn";
 import ETKRegistrationLinkConfirmation from "../../../components/RegistrationLink/Confirmation";
