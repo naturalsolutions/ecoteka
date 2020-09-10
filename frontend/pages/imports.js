e6876d7d6b9ad523688a0bff87c101df8c6ef08d
Javier Blanco <javier_blanco@natural-solutions.eu>
2020-09-10 16:52:13 +0200

    wip

--- frontend/pages/imports.js
+++ frontend/pages/imports.js
@@ -1,9 +1,62 @@
+import { makeStyles } from "@material-ui/core/styles";
 import Template from "../components/Template";
+import {
+  Grid,
+  Typography,
+  Paper,
+  Button,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  grid: {
+    height: "100%",
+  },
+  paper: {
+    height: "100%",
+    padding: "1rem",
+  },
+});
 
 export default function ImportsPage() {
+  const classes = useStyles();
+
   return (
     <Template>
-      <div>Hello import !</div>
+      <Grid container direction="column" className={classes.grid}>
+        <Paper square className={classes.paper}>
+          <Grid container>
+            <Grid item xs={6}>
+              <Typography variant="h5">Historique des imports</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Grid xs={12} container direction="row-reverse">
+                <Button variant="contained" color="primary">
+                  Importer
+                </Button>
+                <Button variant="contained" color="primary">
+                  Supprimer
+                </Button>
+              </Grid>
+            </Grid>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Nom du fichier</TableCell>
+                    <TableCell>Date de l'import</TableCell>
+                    <TableCell>Status de l'import</TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Paper>
+      </Grid>
     </Template>
   );
 }
