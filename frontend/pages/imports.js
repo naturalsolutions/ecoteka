d5dc2e3df0fe5c908752774ec093a6e8c142a47d
Javier Blanco <javier_blanco@natural-solutions.eu>
2020-09-11 11:36:43 +0200

    import: check user is logged

--- frontend/pages/imports.js
+++ frontend/pages/imports.js
@@ -1,23 +1,35 @@
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import Template from "../components/Template";
 import ETKImportHistory from "../components/Import/History/Index";
-
+import { useAppContext } from "../providers/AppContext";
 import { apiRest } from "../lib/api";
 
 export default function ImportsPage() {
+  const { user } = useAppContext();
+  const router = useRouter();
   const [rows, setRows] = useState([]);
   const headers = ["Nom du fichier", "Date de l'import", "Status de l'import"];
 
-  useEffect(async () => {
-    const rows = await apiRest.geofiles.getAll();
+  useEffect(() => {
+    if (!user) {
+      router.push("/");
+    } else {
+      try {
+        async function fetchData() {
+          const rows = await apiRest.geofiles.getAll();
+          setRows(rows);
+        }
 
-    setRows(rows);
+        fetchData();
+      } catch (e) {}
+    }
   }, []);
 
   return (
     <Template>
-      <ETKImportHistory headers={headers} rows={rows} />
+      {user && <ETKImportHistory headers={headers} rows={rows} />}
     </Template>
   );
 }
