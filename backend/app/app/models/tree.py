b70031babf9170c9c772c6c1a2c17267d7fdc7e6
MV <el-makki_voundy@natural-solutions.eu>
2020-09-17 18:49:19 +0200

    tree post

--- backend/app/app/models/tree.py
+++ backend/app/app/models/tree.py
@@ -1,15 +1,26 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, inspect
 from sqlalchemy.dialects.postgresql import JSONB
 from geoalchemy2 import Geometry
+from geoalchemy2.shape import to_shape
 from sqlalchemy.orm import relationship
-
+from app.schemas.tree import Tree_xy
 from app.db.base_class import Base
 
 
 class Tree(Base):
     id = Column(Integer, primary_key=True, index=True)
     geofile_id = Column(Integer, ForeignKey('geofile.id'))
+    scientific_name = Column(String, nullable=True)
+    taxref_id = Column(Integer, nullable=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     organization_id = Column(Integer, ForeignKey('organization.id'))
     geom = Column('geom', Geometry('POINT'))
     properties = Column(JSONB, nullable=True)
+
+    def to_xy(self):
+        coords = to_shape(self.geom)
+        return Tree_xy(
+            **{c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs if c.key != 'geom'},
+            x = coords.x,
+            y = coords.y
+        )
--- backend/app/app/schemas/tree.py
+++ backend/app/app/schemas/tree.py
@@ -1,21 +1,33 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, Any
 
 from pydantic import BaseModel, Json
+from sqlalchemy import inspect
 
+class TreePost(BaseModel):
+    ''' Schema for tree creation post request data'''
+    x: int
+    y: int
+    scientific_name: Optional[str]
+    taxref_id: Optional[int]
 
-class TreeBase(BaseModel):
-    name: str
-    geo_file: int
+class TreeMeta(BaseModel):
+    scientific_name: Optional[str]
+    taxref_id: Optional[int]
+    properties: Json
+    geofile_id: Optional[int]
     user_id: int
     organization_id: int
-    geom: str
-    properties: Json
 
+class Tree_xy(TreePost, TreeMeta):
+    '''Representation of Tree with (x,y) coords instead of geoalchemy.geom-point'''
+    id: int
+
+class TreeBase(TreeMeta):
+    geom: Any
 
 class TreeCreate(TreeBase):
     pass
-
 
 class TreeUpdate(BaseModel):
     pass
