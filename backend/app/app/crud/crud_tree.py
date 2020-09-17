71c038fab056047424d648e21cb5dd10e9a49fa6
Javier Blanco <javier_blanco@natural-solutions.eu>
2020-09-17 09:13:01 +0200

    remove trees before delete geofile

--- backend/app/app/crud/crud_tree.py
+++ backend/app/app/crud/crud_tree.py
@@ -5,12 +5,15 @@ from sqlalchemy.orm import Session
 
 from app.db.session import engine
 from app.crud.base import CRUDBase
-from app.models.tree import Tree
-from app.schemas.tree import TreeCreate, TreeUpdate
+from app.models import Tree, GeoFile
+from app.schemas import TreeCreate, TreeUpdate
 
 
 class CRUDTree(CRUDBase[Tree, TreeCreate, TreeUpdate]):
-    pass
+    def remove_from_geofile(self, db: Session, *, geofile_id: int):
+        db.query(Tree).filter(Tree.geofile_id == geofile_id).delete()
+        db.commit()
+        return
 
 
 tree = CRUDTree(Tree)
