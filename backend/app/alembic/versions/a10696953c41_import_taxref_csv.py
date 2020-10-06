7a9d133db1f1f99f31e3655389f96c6cade95e55
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-10-06 11:01:13 +0200

    change import csv , now unzip and import file in the same instructions

--- backend/app/alembic/versions/a10696953c41_import_taxref_csv.py
+++ backend/app/alembic/versions/a10696953c41_import_taxref_csv.py
@@ -8,6 +8,7 @@ Create Date: 2020-09-30 21:30:57.374710
 from alembic import op
 import sqlalchemy as sa
 import os
+import gzip
 
 
 # revision identifiers, used by Alembic.
@@ -22,12 +23,11 @@ tableTarget = 'taxref'
 def upgrade():
     connectionObj = op.get_bind()
     rawCursor = connectionObj.connection.cursor()
-    filename = os.path.join('/data', 'TAXREFv13.txt')
-    with open(filename, "r") as csvFile:
+    filenameZip = os.path.join('/data', 'TAXREFv13.txt.gz')
+
+    with gzip.open(filenameZip, 'rb') as csvFile:
         rawCursor.copy_expert(fr"COPY {tableTarget} FROM STDIN DELIMITER E'\t' CSV HEADER;", csvFile)
-    pass
 
 
 def downgrade():
     op.execute(f"TRUNCATE TABLE {tableTarget};", execution_options=None)
-    pass
