61fcb105fd7ac042faca5fae092d54bb3b6c03ec
Javier Blanco <javier_blanco@natural-solutions.eu>
2020-10-15 18:06:07 +0200

    fixed

--- frontend/components/SignIn/Button.tsx
+++ frontend/components/SignIn/Button.tsx
@@ -1,13 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { Button, ButtonProps } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { useTemplate } from "../Template";
 import ETKFormSignIn, { ETKFormSignInActions } from "./Form";
 
-const ETKSignInButton: React.FC<ButtonProps> = (props) => {
+interface ETKFormSignInProps extends ButtonProps {
+  open: boolean;
+  dialogTitle?: string;
+}
+
+const defaultProps: ETKFormSignInProps = {
+  open: false,
+};
+
+const ETKSignInButton: React.FC<ETKFormSignInProps> = (props) => {
   const { t } = useTranslation("components");
   const { dialog } = useTemplate();
   const formRef = useRef<ETKFormSignInActions>();
+
+  const { dialogTitle, open, ...buttonProps } = props;
 
   const onButtonClick = () => {
     const dialogActions = [
@@ -30,7 +41,7 @@ const ETKSignInButton: React.FC<ButtonProps> = (props) => {
     ];
 
     dialog.open({
-      title: t("SignIn.title"),
+      title: dialogTitle || t("SignIn.title"),
       content: <ETKFormSignIn ref={formRef} />,
       actions: dialogActions,
       dialogProps: {
@@ -41,11 +52,23 @@ const ETKSignInButton: React.FC<ButtonProps> = (props) => {
     });
   };
 
+  useEffect(() => {
+    if (open && dialog) {
+      onButtonClick();
+    }
+  }, [dialog]);
+
   return (
-    <Button onClick={onButtonClick} {...props}>
+    <Button
+      onClick={onButtonClick}
+      style={{ display: open ? "none" : "block" }}
+      {...buttonProps}
+    >
       {t("SignIn.buttonConnexion")}
     </Button>
   );
 };
+
+ETKSignInButton.defaultProps = defaultProps;
 
 export default ETKSignInButton;
