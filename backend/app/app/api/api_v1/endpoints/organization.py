686185d1687b8ba325d2d35ef20f9da73dd82458
mvoundy <el-makki_voundy@natural-solutions.eu>
2020-11-17 13:13:31 +0000

    organization end-points Co-authored-by: Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>

--- backend/app/app/api/api_v1/endpoints/organization.py
+++ backend/app/app/api/api_v1/endpoints/organization.py
@@ -21,36 +21,71 @@ from app.core import security
 
 router = APIRouter()
 
+@router.post('/', response_model=schemas.Organization)
+def create_organization (
+    *,
+    db: Session = Depends(deps.get_db),
+    organization: schemas.OrganizationCreate,
+    current_user: models.User = Depends(deps.get_current_active_user)
+):
+    return crud.organization.create(db, obj_in=organization).to_schema()
 
-@router.get("/{organization_id}")
+@router.patch("/{id}", response_model=schemas.Organization)
+def update_organization(
+    id: int,
+    *,
+    db: Session = Depends(deps.get_db),
+    current_user: models.User = Depends(deps.get_current_user),
+    organization: schemas.OrganizationUpdate
+):
+    org = crud.organization.get(db, id=id)
+
+    if not org:
+        raise HTTPException(status_code=404, detail="Organization not found")
+
+    return crud.organization.update(
+        db,
+        db_obj=org,
+        obj_in=jsonable_encoder(organization, exclude_unset=True)
+    )
+
+@router.get("/{id}")
 def get_organization_by_id(
     *,
-    organization_id: int,
+    id: int,
     db: Session = Depends(deps.get_db)
 ) -> Optional[schemas.Organization]:
     """
     get one organization by id
     """
-    organization_in_db = crud.organization.get(db, id=organization_id)
+    organization_in_db = crud.organization.get(db, id=id)
 
     if not organization_in_db:
         raise HTTPException(status_code=404, detail="Organization not found")
 
     total_trees = crud.organization.get_total_tree_by_id(
-        db, id=organization_id)
+        db, id=id)
 
     if not total_trees:
         total_trees = 0
 
     organization_response = schemas.Organization(
-        id=organization_in_db.id,
-        name=organization_in_db.name,
-        slug=organization_in_db.slug,
-        total_trees=total_trees
+        **jsonable_encoder(organization_in_db.to_schema(), exclude=['total_trees']),
+        total_trees = total_trees
     )
 
     return organization_response
 
+@router.get("/{id}/teams", response_model=List[schemas.Organization])
+def get_teams(
+    id: int,
+    *,
+    db: Session = Depends(deps.get_db),
+    current_user: models.User = Depends(deps.get_current_user)
+):
+    return [
+        org.to_schema() for org in crud.organization.get_teams(db, parent_id=id)
+    ]
 
 @router.get("/geojson/{organization_id}")
 def generate_style(
