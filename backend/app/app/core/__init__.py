d537eaba77044464c935d22ee0f2f330385a964a
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-11-19 14:45:23 +0100

    fix: new method declaration in module

--- backend/app/app/core/__init__.py
+++ backend/app/app/core/__init__.py
@@ -9,14 +9,18 @@ from .exceptions_handlers import (
     authjwt_exception_handler
 )
 from .security import (
+    authorization,
     generate_refresh_token_response,
     get_current_user,
     get_current_active_user,
     get_current_user_if_is_superuser,
-    get_optional_current_active_user
+    get_optional_current_active_user,
+    enforcer,
+    set_policies
 )
 
 __all__ = [
+    "authorization",
     "authjwt_exception_handler",
     "generate_refresh_token_response",
     "get_current_active_user",
@@ -26,5 +30,7 @@ __all__ = [
     "get_password_hash",
     "settings",
     "verify_password",
-    "authorization"
+    "authorization",
+    'enforcer',
+    'set_policies'
 ]
