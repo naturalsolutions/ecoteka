38d6f9246101daf7ac29370e1c90e63d66173db2
Javi Blanco <javier_blanco@natural-solutions.eu>
2020-11-19 22:58:46 +0100

    add map to tab/generalinfotab

--- frontend/components/Organization/Tab/GeneralInfoTab.tsx
+++ frontend/components/Organization/Tab/GeneralInfoTab.tsx
@@ -1,13 +1,33 @@
 import { TOrganization } from "@/pages/organization/[id]";
 import React, { FC } from "react";
-import { Box } from "@material-ui/core";
+import { Grid, Box } from "@material-ui/core";
+import Map from "@/components/Map/Map";
+import { apiRest } from "@/lib/api";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  map: {
+    minHeight: "400px",
+  },
+}));
 
 interface IGeneralInfoTab {
   organization: TOrganization;
 }
 
 const GeneralInfoTab: FC<IGeneralInfoTab> = ({ organization }) => {
-  return <Box>General Infos</Box>;
+  const classes = useStyles();
+
+  return (
+    <Grid container alignItems="stretch">
+      <Grid item xs={6}>
+        {organization.name}
+      </Grid>
+      <Grid item xs={6} className={classes.map}>
+        <Map styleSource={`/api/v1/maps/style?token=${apiRest.getToken()}`} />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default GeneralInfoTab;
