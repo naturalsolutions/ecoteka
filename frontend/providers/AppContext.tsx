7aeab5e2c1dce7af697df14bc82c966d689a20e8
Javi Blanco <javier_blanco@natural-solutions.eu>
2020-11-26 10:36:26 +0100

    frontend: simplified AppContext

--- frontend/providers/AppContext.tsx
+++ frontend/providers/AppContext.tsx
@@ -6,30 +6,8 @@ import useLocalStorage from "@/lib/hooks/useLocalStorage";
 const StoreContext = createContext({} as any);
 
 export const Provider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useLocalStorage<IUser>("user", null);
-
-  useEffect(() => {
-    if (!user) {
-      api.users
-        .me()
-        .then((currentUser: IUser) => {
-          if (currentUser.organizations.length === 1) {
-            currentUser.currentOrganization = currentUser.organizations[0];
-          }
-
-          setUser(currentUser);
-        })
-        .catch((e) => {
-          setUser(null);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    } else {
-      setIsLoading(false);
-    }
-  }, []);
 
   return (
     <StoreContext.Provider value={{ user, setUser, isLoading }}>
