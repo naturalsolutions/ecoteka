f09795e8480e8174a52bde9fbaa06df5a7c157b2
Gaëtan <gaetan_duhamel@natural-solutions.eu>
2020-12-01 11:57:41 +0100

    fix migration 2

--- backend/app/alembic/versions/35d1666263c4_add_column_archived_and_archived_at_to_.py
+++ backend/app/alembic/versions/35d1666263c4_add_column_archived_and_archived_at_to_.py
@@ -18,11 +18,8 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('organization',
-    Column('archived', sa.Boolean(), nullable=False, server_default=expression.true())
-    op.add_column('organization',
-    Column('archived_at', sa.DateTime(), nullable=True, server_default=func.current_timestamp())
-)
+    op.add_column('organization', sa.Column('archived', sa.Boolean(), nullable=False, server_default=expression.true()))
+    op.add_column('organization', sa.Column('archived_at', sa.DateTime(), nullable=True, server_default=func.current_timestamp()))
 
 def downgrade():
     op.drop_column("organization", "archived")
