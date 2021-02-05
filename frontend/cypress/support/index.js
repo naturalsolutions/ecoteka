93ac0ad0bd9d1a76e3d6a4de2aa3a1eb14f724a2
Gaëtan DUHAMEL <gaetan_duhamel@natural-solutions.eu>
2021-02-05 10:24:33 +0000

    Resolve "Add Cypress tests for edition page"

--- frontend/cypress/support/index.js
+++ frontend/cypress/support/index.js
@@ -13,8 +13,10 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+// Cypress.on("uncaught:exception", (err, runnable) => {
+//   // returning false here prevents Cypress from
+//   // failing the test
+//   return false;
+// });
