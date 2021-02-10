abdc057a4f42d7c0c819289751960e93ebda2114
Javi Blanco <javier_blanco@natural-solutions.eu>
2021-02-10 13:37:45 +0100

    refact: 100vh map

--- frontend/components/AppLayout/Carto.tsx
+++ frontend/components/AppLayout/Carto.tsx
@@ -3,35 +3,6 @@ import { makeStyles, Drawer } from "@material-ui/core";
 import { TMapToolbarAction } from "@/components/Map/Toolbar";
 import { use100vh } from "react-div-100vh";
 
-const useStyles = makeStyles((theme) => ({
-  content: {
-    position: "relative",
-    backgroundColor: theme.palette.background.default,
-    height: (props) => props.appHeight,
-    flexGrow: 1,
-  },
-  drawerLeft: {
-    // @ts-ignore
-    width: (props) => props.drawerLeftWidth,
-    height: (props) => props.appHeight,
-    flexShrink: 0,
-    [theme.breakpoints.down("sm")]: {
-      maxWidth: 300,
-    },
-  },
-
-  drawerLeftPaper: {
-    // @ts-ignore
-    width: (props) => props.drawerLeftWidth,
-    padding: "1rem",
-    height: (props) => props.appHeight,
-    marginTop: 48,
-    [theme.breakpoints.down("sm")]: {
-      maxWidth: 300,
-    },
-  },
-}));
-
 const AppLayoutCartoContext = createContext(null);
 
 export const useAppLayoutCarto = () => useContext(AppLayoutCartoContext);
@@ -50,10 +21,35 @@ const AppLayoutCarto: FC<IAppLayoutCarto> = ({
   const toolbarHeight = 48;
   const vh = use100vh();
   const appHeight = vh - toolbarHeight;
-  const classes = useStyles({
-    drawerLeftWidth,
-    appHeight,
-  });
+  const useStyles = makeStyles((theme) => ({
+    content: {
+      position: "relative",
+      backgroundColor: theme.palette.background.default,
+      height: appHeight,
+      flexGrow: 1,
+    },
+    drawerLeft: {
+      // @ts-ignore
+      width: drawerLeftWidth,
+      height: appHeight,
+      flexShrink: 0,
+      [theme.breakpoints.down("sm")]: {
+        maxWidth: 300,
+      },
+    },
+
+    drawerLeftPaper: {
+      // @ts-ignore
+      width: drawerLeftWidth,
+      padding: "1rem",
+      height: appHeight,
+      marginTop: 48,
+      [theme.breakpoints.down("sm")]: {
+        maxWidth: 300,
+      },
+    },
+  }));
+  const classes = useStyles();
 
   return (
     <AppLayoutCartoContext.Provider value={{}}>
