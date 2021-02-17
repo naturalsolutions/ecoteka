78a29f7e3ffec73ce1abaa6758aabd30be39e357
Gaëtan DUHAMEL <gaetan_duhamel@natural-solutions.eu>
2021-02-17 13:08:23 +0000

    Resolve "Add Map Interventions filters"

--- backend/app/app/api/api_v1/endpoints/maps.py
+++ backend/app/app/api/api_v1/endpoints/maps.py
@@ -1,8 +1,10 @@
 from typing import Dict, Optional, List
 import json
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from sqlalchemy.orm import Session
 from bokeh import palettes
+import geopandas as gpd
+from shapely.geometry import box
 
 from app.api import get_db
 from app.core import (
@@ -10,10 +12,13 @@ from app.core import (
     set_policies
 )
 
+from app.crud import organization
+
 router = APIRouter()
 
 policies = {
-    "maps:get_filters": ["owner", "manager", "contributor", "reader"],
+    "maps:get_geojson": ["owner", "manager", "contributor", "reader"],
+    "maps:get_geojson": ["owner", "manager", "contributor", "reader"],
     "maps:get_bbox": ["owner", "manager", "contributor", "reader"]
 }
 
@@ -107,6 +112,158 @@ def complementaryColor(my_hex):
     comp = ['%02X' % (255 - int(a, 16)) for a in rgb]
     
     return ''.join(comp)
+
+@router.get("/geojson", dependencies=[Depends(authorization("maps:get_geojson"))])
+def get_geojson(
+    organization_id: int,
+    mode: Optional[str] = "geopandas",
+    db: Session = Depends(get_db)
+) -> Dict: 
+    """
+    Get Organization GeoJSON
+    """
+    organization_in_db = organization.get(db, id=organization_id)
+
+    if not organization_in_db:
+        raise HTTPException(status_code=404, detail="Organization not found")
+
+    response = None
+    if mode == 'geopandas':
+        sql = f"""SELECT t.id as id, t.properties, geom, 
+                        COALESCE(json_agg(json_build_object('id', i.id,
+                                                    'date', i.date,
+                                                    'intervention_end_date', i.intervention_end_date,
+                                                    'intervention_start_date', i.intervention_start_date,
+                                                    'intervenant', i.intervenant,
+                                                    'required_documents', i.required_documents,
+                                                    'required_material', i.required_material,
+                                                    'estimated_cost', i.estimated_cost, 
+                                                    'intervention_type', i."intervention_type", 
+                                                    'properties', i."properties"
+                                                    )) FILTER (WHERE i.id IS NOT NULL), '[]'
+                                    ) as interventions  
+                    FROM public.tree t LEFT JOIN intervention i 
+                    ON i.tree_id = t.id
+                    WHERE t.organization_id = {organization_in_db.id}
+                    GROUP BY t.id, i.id
+                """
+        df = gpd.read_postgis(sql, db.bind)
+        data = df.to_json()
+        response = json.loads(data)
+    else:
+        sql = f"""SELECT jsonb_build_object('type','FeatureCollection', 'features', jsonb_agg(features.feature))
+                   FROM (
+                       SELECT jsonb_build_object(
+                           'type',       'Feature',
+                           'id',         id,
+                           'geometry',   ST_AsGeoJSON(geom)::jsonb,
+                           'properties', to_jsonb(selected_trees) - 'geom'
+                   ) AS feature
+                   FROM (
+                       SELECT t.id as id, t.properties, geom, 
+                        COALESCE(json_agg(json_build_object('id', i.id,
+                                                    'date', i.date,
+                                                    'intervention_end_date', i.intervention_end_date,
+                                                    'intervention_start_date', i.intervention_start_date,
+                                                    'intervenant', i.intervenant,
+                                                    'required_documents', i.required_documents,
+                                                    'required_material', i.required_material,
+                                                    'estimated_cost', i.estimated_cost, 
+                                                    'intervention_type', i."intervention_type", 
+                                                    'properties', i."properties"
+                                                    )) FILTER (WHERE i.id IS NOT NULL), '[]'
+                                    ) as interventions  
+                    FROM public.tree t LEFT JOIN intervention i 
+                    ON i.tree_id = t.id
+                    WHERE t.organization_id = {organization_in_db.id}
+                    GROUP BY t.id, i.id
+                       ) 
+                       selected_trees) 
+                   features;
+            """
+        res = db.execute(sql)
+        row = res.first()
+        response = dict(row.jsonb_build_object)
+    return response
+
+@router.get("/geobuf", dependencies=[Depends(authorization("maps:get_geojson"))])
+def get_geobuff(
+    organization_id: int,
+    db: Session = Depends(get_db)
+)-> Dict:
+    """
+    Generate a compressed Feature Collection for Organization trees
+    """
+
+    organization_in_db = organization.get(db, id=organization_id)
+
+    if not organization_in_db:
+        raise HTTPException(status_code=404, detail="Organization not found")
+
+    sql = f"""
+
+        WITH selection AS (
+            SELECT t.id as id, t.properties, geom, 
+                        COALESCE(json_agg(json_build_object('id', i.id,
+                                                    'date', i.date,
+                                                    'intervention_end_date', i.intervention_end_date,
+                                                    'intervention_start_date', i.intervention_start_date,
+                                                    'intervenant', i.intervenant,
+                                                    'required_documents', i.required_documents,
+                                                    'required_material', i.required_material,
+                                                    'estimated_cost', i.estimated_cost, 
+                                                    'intervention_type', i."intervention_type", 
+                                                    'properties', i."properties"
+                                                    )) FILTER (WHERE i.id IS NOT NULL), '[]'
+                                    ) as interventions  
+                    FROM public.tree t LEFT JOIN intervention i 
+                    ON i.tree_id = t.id
+                    WHERE t.organization_id = {organization_in_db.id}
+                    GROUP BY t.id, i.id
+        )
+        SELECT encode(ST_AsGeobuf(selection.*), 'base64') AS pbf
+        FROM  selection;
+    """
+
+    res = db.execute(sql)
+    row = res.first()
+    # return Response(content=row, media_type="application/vnd.google.protobuf")
+    return row
+
+
+@router.get("/{organization_id}/tree_tiles/{z}/{x}/{y}/.pbf", dependencies=[Depends(authorization("maps:get_geojson"))])
+def get_tree_tiles(
+    z: int,
+    x: float,
+    y: float,
+    *,
+    organization_id: int,
+    db: Session = Depends(get_db)
+) -> Dict:
+    """
+    Generate Trees MVT for a given organization
+    """
+    organization_in_db = organization.get(db, id=organization_id)
+
+    if not organization_in_db:
+        raise HTTPException(status_code=404, detail="Organization not found")
+    
+    sql = f"""
+        WITH mvtgeom AS (
+            SELECT
+                ST_AsMVTGeom(ST_SetSRID(geom, 4326), ST_Transform(ST_TileEnvelope({z},{x},{y}), 4326)) AS geom,
+                id,
+                properties
+            FROM public.tree AS t
+            WHERE ST_Intersects(ST_SetSRID(geom, 4326), ST_Transform(ST_TileEnvelope({z},{x},{y}), 4326)) AND t.organization_id = {organization_in_db.id}
+            )
+        SELECT ST_AsMVT(mvtgeom.*) AS mvt
+        FROM mvtgeom;
+    """
+    result_proxy = db.execute(sql)
+    return result_proxy.first()
+
+
 
 @router.get("/filter", dependencies=[Depends(authorization("maps:get_filters"))])
 def get_filters(
