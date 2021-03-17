118a815ec68015cd916da72952b43047b3eb6756
Gaëtan <gaetan_duhamel@natural-solutions.eu>
2021-03-17 10:02:46 +0100

    fix: tmp ts-nocheck on Button gradientStyles

--- frontend/styles/Button/gradient/index.tsx
+++ frontend/styles/Button/gradient/index.tsx
@@ -1,8 +1,9 @@
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import gradientBtnStyles from './gradientBtn.styles';
+// @ts-nocheck
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import gradientBtnStyles from "./gradientBtn.styles";
 
 const useGradientBtnStyles = makeStyles(gradientBtnStyles);
 
 export { gradientBtnStyles, useGradientBtnStyles };
 
-export { default } from './gradientBtn.styles';
+export { default } from "./gradientBtn.styles";
