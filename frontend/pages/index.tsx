13f53ae89576d1661a5b84c411f3bdb7a7eda23b
Javi Blanco <javier_blanco@natural-solutions.eu>
2021-04-19 12:17:56 +0200

    feat: add OSMTrees on home page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,7 @@
+import { FC, useEffect, useState } from "react";
 import AppLayoutGeneral from "@/components/AppLayout/General";
-import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
-import { Typography, Container, Box, Grid } from "@material-ui/core";
+import { Typography, Container, Box, Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useAppContext } from "@/providers/AppContext";
 import TutorialsGallery from "@/components/OrganizationV2/Tutorials";
@@ -9,7 +9,8 @@ import DatasetsGallery from "@/components/Home/DatasetsGallery";
 import UserOrganizationGallery from "@/components/Home/UserOrganizationGallery";
 import CallToLogin from "@/components/Core/CallToActions/CallToLogin";
 import AddOrganization from "@/components/Core/CallToActions/AddOrganization";
-import React from "react";
+import MapProvider, { useMapContext } from "@/components/Map/Provider";
+import OSMLayer from "@/components/Map/Layers/OSM";
 
 const useStyles = makeStyles(({ spacing }) => ({
   subtitle: {
@@ -21,16 +22,29 @@ const useStyles = makeStyles(({ spacing }) => ({
   },
 }));
 
-const Home: React.FC = () => {
+const Home: FC = () => {
   const styles = useStyles();
   const { user } = useAppContext();
   const { t } = useTranslation(["common", "components"]);
+  const [layers, setLayers] = useState<[]>();
+
+  useEffect(() => {
+    if (!layers?.length) {
+      const osmLayer = OSMLayer(true);
+
+      setLayers(osmLayer);
+    }
+  }, [layers]);
+
   return (
     <AppLayoutGeneral>
       <Container>
         <Typography variant="h4" component="h1" color="textPrimary">
           {t("components.Home.title")}
         </Typography>
+        <Box mt={4}>
+          <MapProvider layers={layers} borderRadius={20} height={300} />
+        </Box>
         <Typography
           variant="h6"
           component="h2"
