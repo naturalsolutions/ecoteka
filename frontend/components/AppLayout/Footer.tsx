cfe4b76331da26a45c6121b7d681222c2f84d02f
Javi Blanco <javier_blanco@natural-solutions.eu>
2021-05-03 14:27:00 +0200

    fix: add links to footer

--- frontend/components/AppLayout/Footer.tsx
+++ frontend/components/AppLayout/Footer.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
-import { makeStyles, Container, Grid, Link } from "@material-ui/core";
+import {
+  makeStyles,
+  Container,
+  Grid,
+  Link,
+  Typography,
+} from "@material-ui/core";
 import { List } from "@material-ui/core";
 import { ListItem } from "@material-ui/core";
 import { EcotekaTheme } from "@/theme/config";
-import NewsletterButton from "../NewsletterButton";
+import NewsletterButton from "@/components/NewsletterButton";
+import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles((theme: EcotekaTheme) => ({
   root: {
@@ -15,8 +22,29 @@ const useStyles = makeStyles((theme: EcotekaTheme) => ({
   },
 }));
 
+export type FooterTitleColumn = "ns" | "ecoteka" | "newsletter";
+export type FooterLinks = "community" | "gitlab" | "avion" | "ns" | "team";
+
+export interface FooterLink {
+  href: string;
+  title: string;
+}
+
 const LayoutFooter: FC = () => {
   const classes = useStyles();
+  const { t } = useTranslation();
+  const titles: Record<FooterTitleColumn, string> = t(
+    "components.LayoutFooter.titleColumn",
+    {
+      returnObjects: true,
+    }
+  );
+  const links: Record<FooterLinks, FooterLink> = t(
+    "components.LayoutFooter.links",
+    {
+      returnObjects: true,
+    }
+  );
 
   return (
     <div className={classes.root}>
@@ -25,19 +53,42 @@ const LayoutFooter: FC = () => {
           <Grid item xs={12} md={4}>
             <List>
               <ListItem>
-                <Link className={classes.text}>En savoir plus sur ecoTeka</Link>
+                <Typography variant="subtitle2" className={classes.text}>
+                  {titles.ecoteka}
+                </Typography>
               </ListItem>
               <ListItem>
-                <Link className={classes.text}>
-                  {`Plus qu\u0027un produit, une communauté`}
+                <Link href={links.community.href} className={classes.text}>
+                  {links.community.title}
                 </Link>
               </ListItem>
               <ListItem>
-                <Link className={classes.text}>Retrouvez-nous sur Gitlab</Link>
+                <Link href={links.gitlab.href} className={classes.text}>
+                  {links.gitlab.title}
+                </Link>
               </ListItem>
               <ListItem>
-                <Link className={classes.text}>
-                  Le projet maintenant et à venir
+                <Link href={links.avion.href} className={classes.text}>
+                  {links.avion.title}
+                </Link>
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <List>
+              <ListItem>
+                <Typography variant="subtitle2" className={classes.text}>
+                  {titles.ns}
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Link href={links.ns.href} className={classes.text}>
+                  {links.ns.title}
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href={links.team.href} className={classes.text}>
+                  {links.team.title}
                 </Link>
               </ListItem>
             </List>
@@ -45,25 +96,10 @@ const LayoutFooter: FC = () => {
           <Grid item xs={12} md={4}>
             <List>
               <ListItem>
-                <Link className={classes.text}>
-                  En savoir plus sur Natural Solutions
-                </Link>
+                <Typography variant="subtitle2" className={classes.text}>
+                  {titles.newsletter}
+                </Typography>
               </ListItem>
-              <ListItem>
-                <Link className={classes.text}>Site internet</Link>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.text}>{`L\u0027équipe`}</Link>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.text}>
-                  {`Suivre l\u0027actualité sur les écosystèmes urbains`}
-                </Link>
-              </ListItem>
-            </List>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <List>
               <ListItem>
                 <NewsletterButton />
               </ListItem>
