876e49e9463e52f5f4afea416d0632cbfb18590a
Gaëtan <gaetan_duhamel@natural-solutions.eu>
2021-06-25 16:10:36 +0200

    fix(frontend): add greening simple metrics

---
+++ frontend/components/Core/Metrics/SimpleMetric.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import { makeStyles, Theme, Grid, Typography } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
+
+export interface SimpleMetricProps {
+  metric: number;
+  caption: string;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    paddingTop: theme.spacing(3),
+  },
+  caption: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const SimpleMetric: FC<SimpleMetricProps> = ({ metric, caption }) => {
+  const classes = useStyles();
+  const { t } = useTranslation(["common"]);
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={classes.root}
+    >
+      <Grid item>
+        <Typography variant="h3" component="div">
+          {metric == 0 ? t("common.nonAvailableAbbr") : metric}
+        </Typography>
+      </Grid>
+      <Grid item className={classes.caption}>
+        {caption}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SimpleMetric;
