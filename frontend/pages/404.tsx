ceb682921e90ebae3a65128d18748b5234434dbc
Gaëtan <gaetan_duhamel@natural-solutions.eu>
2021-08-21 02:33:40 +0200

    fix: change component on error pages

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,21 +1,5 @@
-import ErrorPage from "@/components/ErrorPage";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  figcaption: {
-    fontSize: "18px",
-    padding: "20px",
-  },
-}));
+import Error from "@/components/Core/Error";
 
 export default function Page404() {
-  const classes = useStyles();
-
-  return (
-    <ErrorPage>
-      <figcaption className={classes.figcaption}>
-        Aucun arbre n'a encore poussé sur cette page ...
-      </figcaption>
-    </ErrorPage>
-  );
+  return <Error errorCode={404} />;
 }
