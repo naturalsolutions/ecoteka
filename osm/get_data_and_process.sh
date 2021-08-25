b6bb9a40ab8f6d38cff4e4e237e04529a5da4491
Javier Blanco <javier_blanco@natural-solutions.eu>
2021-08-25 10:04:32 +0200

    build: update osm build

--- osm/get_data_and_process.sh
+++ osm/get_data_and_process.sh
@@ -3,7 +3,7 @@
 mkdir /data
 
 # get data from osm
-ogr2ogr -f GeoJSON /data/planet.geojson /vsicurl_streaming/https://mirror.init7.net/openstreetmap/pbf/planet-latest.osm.pbf -sql "select * from points where hstore_get_value(other_tags, 'natural') = 'tree'"
+ogr2ogr -f GeoJSON /data/planet.geojson /vsicurl_streaming/https://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-latest.osm.pbf -sql "select * from points where hstore_get_value(other_tags, 'natural') = 'tree'"
 
 # generate tiles with tippecanoe
 tippecanoe -P -l ecoteka-data -o /data/osm_tmp.mbtiles --force --generate-ids --maximum-zoom=g --drop-densest-as-needed --extend-zooms-if-still-dropping /data/planet.geojson
