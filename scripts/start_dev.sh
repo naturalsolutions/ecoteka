ec95607c11bac7b1eb158ec7a658d413716aab94
javier blanco <javier@PORTATIL.localdomain>
2022-02-08 22:33:24 +0100

    build: rename network

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Create the traefik network and if it exists continues
-docker network create traefik 2> /dev/null
-
 # Create .env from .env.example
 docker run --rm -v $(pwd):/data vikingco/jinja2cli .env.example > .env
 
