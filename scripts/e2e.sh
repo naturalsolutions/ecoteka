71c0b4baf0c5deba17f8fabfd5d51514bbbe5c55
Javier Blanco <javier_blanco@natural-solutions.eu>
2022-02-21 17:10:05 +0100

    build: fix script e2e

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-docker-compose -f docker/docker-compose.yml -f docker/docker-compose.override.yml pull
-docker-compose -f docker/docker-compose.yml -f docker/docker-compose.override.yml build --no-cache
-docker-compose -f docker/docker-compose.yml -f docker/docker-compose.override.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.override.yml pull
+docker-compose -f docker-compose.yml -f docker-compose.override.yml build --no-cache
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
 sleep 30
-docker-compose -f docker/docker-compose.yml -f docker/docker-compose.override.yml exec -T e2e npx cypress run --project /app
-docker-compose -f docker/docker-compose.yml -f docker/docker-compose.override.yml down -v --remove-orphans
+docker-compose -f docker-compose.yml -f docker-compose.override.yml exec -T e2e npx cypress run --project /app
+docker-compose -f docker-compose.yml -f docker-compose.override.yml down -v --remove-orphans
